dff426b37d4ccc0ba5dc175a9cf3b0277b88e7e6
alicemollon2 <ajmollon@hotmail.co.uk>
2015-10-27 09:19:12 +0100

    Added portfolio.js

---
+++ js/portfolio.js
@@ -0,0 +1,55 @@
+$(window).load(function () {
+
+	$(".loading").delay(9000).fadeOut(500);
+
+	$("section#images").isotope({ filter: ".illustration" });
+
+
+	var $section = $('#images').isotope({
+
+	})
+
+	$("nav a").on("click", function () {
+
+		var category = $(this).attr("data-category");
+		$("section#images").isotope({ filter: "div." + category });
+
+
+		return false;
+
+	});
+
+	$(".reset").on("click", function () {
+
+	$("section#images").isotope({ filter: "*" });
+
+	$("nav a#illustration").fadeOut();
+
+	return false;
+
+	});
+
+		
+
+
+
+$("div.illustration img").on("click", function () {
+
+	$("div#lightbox").fadeIn(250);
+
+	var imageSrc = $(this).attr("src");
+
+	$("div#lightbox img").attr("src", imageSrc); 
+
+	});
+
+$("div#lightbox").on("click", function () {
+	$("div#lightbox").fadeOut(250);
+});
+
+});
+
+
+
+
+
