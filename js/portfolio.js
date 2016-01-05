5bb054c41251f96c585ccc52382c0d1d92a73b0f
Alice Mollon <alicemollon@Alices-MacBook-Pro.local>
2016-01-05 20:08:35 +0000

    no scroll on load

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,6 +1,10 @@
 $(window).load(function () {
 
+	$('html, body').css({ 'overflow': 'hidden', 'height': '100%' })
+
 	$(".loading").delay(5000).fadeOut(500);
+
+	$('html, body').removeAttr('style')
 
 	$("section#images").isotope({ filter: ".illustration" });
 
